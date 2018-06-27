57eafc713d596e2826be46c750a4f8081260dbcb
Kevin Gentile <Kevin.gentile@spidasoftware.com>
2018-06-27 09:23:41 -0400

    add more read/write

--- decache.py
+++ decache.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
-# Process random data null any cached data prior to the benchmark
+# Process random data in an attempt to randomize any cached data
 import random
 g = []
-for i in range(1, 999999):
+d = []
+for i in range(1, 9999):
     g.append(random.randint(0,i))
+    for j in range(len(g)):
+        d.append(g[j])
 print("done.")
